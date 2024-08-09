175f3ed85d68995f0e0072092b6c407ea5bc7626
regs@acutebusiness.com <regs@acutebusiness.com>
2024-08-09 15:06:24 +0300

    implement mapper between Dtos and entities

---
+++ accounts/src/main/java/com/atawfeek/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,20 @@
+package com.atawfeek.accounts.mapper;
+
+import com.atawfeek.accounts.dto.AccountsDto;
+import com.atawfeek.accounts.entity.Accounts;
+
+public class AccountsMapper {
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+}
--- accounts/src/main/java/com/atawfeek/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/atawfeek/accounts/service/impl/AccountsServiceImpl.java
@@ -3,14 +3,24 @@ package com.atawfeek.accounts.service.impl;
 import org.springframework.stereotype.Service;
 
 import com.atawfeek.accounts.dto.CustomerDto;
+import com.atawfeek.accounts.repository.AccountsRepository;
+import com.atawfeek.accounts.repository.CustomerRepository;
 import com.atawfeek.accounts.service.IAccountsService;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class AccountsServiceImpl implements IAccountsService {
+
+    private CustomerRepository customerRepository;
+    private AccountsRepository accountsRepository;
 
     @Override
     public void createAccount(CustomerDto customerDto) {
         // TODO Auto-generated method stub
+
+
         
     }
 
